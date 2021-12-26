1c8c7f4adc4dcc0a5f250d95a4fbda7b32c92f9f
mrtelekinesis <86800445+mrtelekinesis@users.noreply.github.com>
2021-12-26 18:24:29 +0000

    Update qdisc.sh

--- Networking/qdisc.sh
+++ Networking/qdisc.sh
@@ -2,5 +2,4 @@
 
 NETDEVICE="wlan0"
 
-tc qdisc del dev $NETDEVICE root
-tc qdisc add dev $NETDEVICE root fq_pie limit 5 flows 5 target 5ms tupdate 5ms alpha 8 beta 24 quantum 512b memory_limit 64Mb ecn_prob 25
+tc qdisc replace dev $NETDEVICE root fq_pie limit 5 flows 5 target 5ms tupdate 5ms alpha 8 beta 24 quantum 512b memory_limit 64Mb ecn_prob 25
