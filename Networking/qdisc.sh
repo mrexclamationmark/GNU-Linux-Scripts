f456c714bed96e928f3f30f24fabb7ffe554c5af
mrtelekinesis <86800445+mrtelekinesis@users.noreply.github.com>
2021-12-29 04:52:07 +0000

    Update qdisc.sh

--- Networking/qdisc.sh
+++ Networking/qdisc.sh
@@ -6,4 +6,4 @@ NETDEVICE="wlan0"
 #tc qdisc replace dev $NETDEVICE root fq_pie limit 5 flows 5 target 5ms tupdate 5ms alpha 8 beta 24 quantum 512b memory_limit 64Mb ecn_prob 25
 
 # CAKE
-tc qdisc replace dev $NETDEVICE root cake bandwidth 2Mbit besteffort dual-srchost nonat nowash no-ack-filter split-gso rtt 5ms raw overhead 0
+tc qdisc replace dev $NETDEVICE root cake bandwidth 2Mbit besteffort flowblind nonat nowash no-ack-filter split-gso rtt 5ms raw overhead 0
