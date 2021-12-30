cb7793491e5b6b5db034a6d61b501a82c23b51e1
mrtelekinesis <86800445+mrtelekinesis@users.noreply.github.com>
2021-12-30 21:37:08 +0000

    Delete qdisc.sh

--- Networking/qdisc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NETDEVICE="wlan0"
-
-# FQ-PIE
-#tc qdisc replace dev $NETDEVICE root fq_pie limit 5 flows 5 target 5ms tupdate 5ms alpha 8 beta 24 quantum 512b memory_limit 64Mb ecn_prob 25
-
-# CAKE
-tc qdisc replace dev $NETDEVICE root cake bandwidth 2Mbit besteffort flowblind nonat nowash no-ack-filter split-gso rtt 5ms raw overhead 0
